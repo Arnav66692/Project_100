f2d0ebb71367969f74a0c19497cab118024110e7
Arnav66692 <105764398+Arnav66692@users.noreply.github.com>
2023-10-22 10:16:35 -0700

    Add files via upload

---
+++ Projkect 100/Atm.py
@@ -0,0 +1,7 @@
+class ATM:
+    def _init_(self,cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+    def check_balance(self):
+        print("Your Balance is $50,000") 
+        new_user = ATM(Card_number, pin number)
